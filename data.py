b430fce9accfa79492eef8944a064dde19cdae85
Howard HUANG <hhuang63@ds2.CS.McGill.CA>
2018-02-03 22:14:46 -0500

    Functionify

--- data.py
+++ data.py
@@ -1,39 +1,51 @@
+import os
 import csv
+import dill
 from tqdm import tqdm
 
 sentences_file = "./data/sentences.csv"
 links_file = "./data/links.csv"
 langs = ['eng', 'fra']
+pickled_file = "./data/eng-fra.pkl"
 
-sentence_ids = set()
-sentence_ids_by_lang = dict()
-for l in langs:
-    sentence_ids_by_lang[l] = set()
-with open(sentences_file, "r") as sentences_csvfile:
-    reader = csv.reader(sentences_csvfile, delimiter='\t')
-    for sentence_id, lang, sentence in tqdm(reader):
-        if lang in langs:
-            sentence_ids.add(sentence_id)
-            sentence_ids_by_lang[lang].add(sentence_id)
+def parse_data(sentences_file, links_file, langs):
+    sentence_ids = set()
+    sentence_ids_by_lang = dict()
+    for l in langs:
+        sentence_ids_by_lang[l] = set()
+    with open(sentences_file, "r") as sentences_csvfile:
+        reader = csv.reader(sentences_csvfile, delimiter='\t')
+        for sentence_id, lang, sentence in tqdm(reader):
+            if lang in langs:
+                sentence_ids.add(sentence_id)
+                sentence_ids_by_lang[lang].add(sentence_id)
 
-pairs = []
-translated_sentence_ids = set()
-with open(links_file, "r") as links_csvfile:
-    reader = csv.reader(links_csvfile, delimiter='\t')
-    for id1,id2 in tqdm(reader):
-        if id1 in sentence_ids and id2 in sentence_ids:
-            if id1 in sentence_ids_by_lang[langs[0]]:
-                pairs.append((id1,id2))
-            else:
-                pairs.append((id2,id1))
-            translated_sentence_ids.add(id1)
-            translated_sentence_ids.add(id2)
+    pairs = []
+    translated_sentence_ids = set()
+    with open(links_file, "r") as links_csvfile:
+        reader = csv.reader(links_csvfile, delimiter='\t')
+        for id1,id2 in tqdm(reader):
+            if id1 in sentence_ids and id2 in sentence_ids:
+                if id1 in sentence_ids_by_lang[langs[0]]:
+                    pairs.append((id1,id2))
+                else:
+                    pairs.append((id2,id1))
+                translated_sentence_ids.add(id1)
+                translated_sentence_ids.add(id2)
 
-translated_sentences = dict()
-with open(sentences_file, "r") as sentences_csvfile:
-    reader = csv.reader(sentences_csvfile, delimiter='\t')
-    for sentence_id, lang, sentence in tqdm(reader):
-        if sentence_id in translated_sentence_ids:
-            translated_sentences[sentence_id] = sentence
+    translated_sentences = dict()
+    with open(sentences_file, "r", encoding='utf-8') as sentences_csvfile:
+        reader = csv.reader(sentences_csvfile, delimiter='\t')
+        for sentence_id, lang, sentence in tqdm(reader):
+            if sentence_id in translated_sentence_ids:
+                translated_sentences[sentence_id] = sentence
 
-output = [(translated_sentences[p[0]],translated_sentences[p[1]]) for p in tqdm(pairs)]
+    output = [(translated_sentences[p[0]],translated_sentences[p[1]]) for p in tqdm(pairs)]
+    with open(pickled_file,"wb") as f:
+        dill.dump(output, f)
+
+def get_data():
+    if not os.path.isfile(pickled_file):
+        parse_data(sentences_file, links_file, langs)
+    with open(pickled_file,"rb") as f:
+        return dill.load(f)
